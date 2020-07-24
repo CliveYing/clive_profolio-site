c7ddce17e8fe5bf3c5f24f9221436efbd7501bb6
CliveYing <41674607+CliveYing@users.noreply.github.com>
2020-07-24 12:40:52 +1000

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -22,9 +22,9 @@ class App extends React.Component {
       title:'Clive Ying',
 
       headerLink:[
-        {title:'Home', path:'/'},
-        {title:'About', path:'/about'},
-        {title:'Contact', path: '/contact'}
+        {title:'Home', path:'clive_profolio-site/'},
+        {title:'About', path:'clive_profolio-site/about'},
+        {title:'Contact', path: 'clive_profolio-site/contact'}
       ],
 
       home: {
@@ -58,22 +58,22 @@ class App extends React.Component {
             <Navbar.Toggle className = "border-0" aria-controls = "navbar-toggle"/>
               <Navbar.Collapse id = "navbar-toggle">
                 <Nav className = "auto">
-                  <Link className = "nav-link" to = "/">Home</Link>
-                  <Link className = "nav-link" to = "/about">About</Link>
-                  <Link className = "nav-link" to = "/contact">Contact</Link>
+                  <Link className = "nav-link" to = "clive_profolio-site/">Home</Link>
+                  <Link className = "nav-link" to = "clive_profolio-site/about">About</Link>
+                  <Link className = "nav-link" to = "clive_profolio-site/contact">Contact</Link>
                 </Nav>
               </Navbar.Collapse>
           </Navbar>
 
           {/* This routing section renders pages from pages folder */}
-          <Route path="/" exact render={()=> 
+          <Route path="clive_profolio-site/" exact render={()=> 
               <HomePage title = {this.state.home.title} 
                         subTitle = {this.state.home.subTitle}
                         text = {this.state.home.text} />} />
-          <Route path="/about" exact render={()=> 
+          <Route path="clive_profolio-site/about" exact render={()=> 
               <AboutPage title = {this.state.about.title} 
                          />} />
-          <Route path="/contact" exact render={()=> 
+          <Route path="clive_profolio-site/contact" exact render={()=> 
               <ContactPage title = {this.state.contact.title} 
                          />} />
 
