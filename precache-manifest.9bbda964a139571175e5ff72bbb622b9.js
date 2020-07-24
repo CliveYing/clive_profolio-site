09ab25a406efad7cfe6e6fe43a1a191b61d5ab13
CliveYing <41674607+CliveYing@users.noreply.github.com>
2020-07-24 12:33:23 +1000

    Updates

---
+++ precache-manifest.9bbda964a139571175e5ff72bbb622b9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6806b81006ecdbba5b68ac679868d50d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57aad61c1a11551ae2e2",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "b6ef146fb2304d15026b",
+    "url": "/static/css/main.70a87754.chunk.css"
+  },
+  {
+    "revision": "57aad61c1a11551ae2e2",
+    "url": "/static/js/2.48d4796f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.48d4796f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6ef146fb2304d15026b",
+    "url": "/static/js/main.c13daa7e.chunk.js"
+  },
+  {
+    "revision": "b94ae8ebfeb4fb5d4925",
+    "url": "/static/js/runtime-main.2cb49aa1.js"
+  },
+  {
+    "revision": "bc9075526e63d2e68232f6c8cee9ba79",
+    "url": "/static/media/USYDpic.bc907552.png"
+  },
+  {
+    "revision": "7bb2cc4f4b7fe27932d3f765f776cd0f",
+    "url": "/static/media/clivePic.7bb2cc4f.jpg"
+  },
+  {
+    "revision": "09f5c6ae0d73877c4fc2595170735ff9",
+    "url": "/static/media/githubPic.09f5c6ae.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clive_profolio-site/precache-manifest.dfde550c513cbffa4cfc05caa315fad9.js"
+  "/precache-manifest.9bbda964a139571175e5ff72bbb622b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clive_profolio-site/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
